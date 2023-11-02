3c09642fcefd7d9ab222d49cf1976f44abaf09dd
Kanhaiya kumar <kanhaiya@aetherss.com>
2023-11-02 19:51:03 +0530

    second commit

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const app = express();
 const port = process.env.PORT || 3000; // Set the port
 
 // Define the /home route to return "This is the home page"
-app.get('/home', (req, res) => {
-  res.json({ message: 'This is the home page' });
+app.get("/.netlify/functions/server/home", (req, res) => {
+  res.json({ message: "This is the home page" });
 });
 
 // Start the server
